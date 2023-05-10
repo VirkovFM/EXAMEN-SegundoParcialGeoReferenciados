15bbf075bb4a5332d8580b9046d0a8520e24e575
VirkovFM <vfm72673@udelasalle.edu.mx>
2023-05-09 19:00:49 -0600

    Lista ordenada menor a mayor

--- javascript/mejorRuta.js
+++ javascript/mejorRuta.js
@@ -1,24 +1,76 @@
 const urlParams = new URLSearchParams(window.location.search);
 const ubicacion = urlParams.get('ubicacion');
 console.log(ubicacion)
+const userList = document.getElementById('users-list');
+//var numConsult = 0;
+//var time = 0;
+//var destino = ""
+//var nameBranch = ""
 
 let users = [];
 
-fetch('http://127.0.0.1:4000/api/users/all')
+async function verMejorRuta(){
+    fetch('http://127.0.0.1:4000/api/users/all')
     .then(response => response.json())
     .then(data => {
-        data.forEach(user => {
-            const destinations = user.manager_name;
-            const nameBranch = user.name;
-            const time = 0;
-            console.log(`Destinations for user ${user.id}: ${destinations}`);
-            users.push({destinations, nameBranch, time}); // Agregar los datos al arreglo
-            console.log(users);
+        var numConsult2 = 0
+        data.forEach(async user => {
+            var time = 0
+            var destino = ""
+            var nameBranch = ""
+            destino = user.manager_name;
+            nameBranch = user.name;
+            numConsult2++;
+            //console.log(`Destinations for user ${user.id}: ${destino}`);
+            await calcRoute(time, destino, nameBranch, numConsult2);
+            
+            //users.push({destino, nameBranch, time}); // Agregar los datos al arreglo
         });
-        users.sort((a, b) => a.time - b.time); // Ordenar los elementos del arreglo de menor a mayor
-        console.log(users);
+        //users.sort((a, b) => a.time - b.time); // Ordenar los elementos del arreglo de menor a mayor
+        //console.log(users);
     })
     .catch(error => console.error(error));
+}
+
+
+function ordenar(usersSend){
+    for (let i = 0; i < usersSend.length - 1; i++) {
+      for (let j = 0; j < usersSend.length - i - 1; j++) {
+        let tiempoActual = tiempoAMinutos(usersSend[j].time);
+        let tiempoSiguiente = tiempoAMinutos(usersSend[j + 1].time);
+    
+        if (tiempoActual > tiempoSiguiente) {
+          let temp = usersSend[j];
+          usersSend[j] = usersSend[j + 1];
+          usersSend[j + 1] = temp;
+        }
+      }
+    }
+    
+    // Función para convertir el tiempo a minutos
+    function tiempoAMinutos(tiempo) {
+      let tiempoArray = tiempo.split(' ');
+    
+      let horas = 0;
+      let minutos = 0;
+    
+      for (let i = 0; i < tiempoArray.length; i++) {
+        if (tiempoArray[i] === 'h') {
+          horas = parseInt(tiempoArray[i - 1]);
+        }
+    
+        if (tiempoArray[i] === 'min') {
+          minutos = parseInt(tiempoArray[i - 1]);
+        }
+      }
+    
+      return horas * 60 + minutos;
+    }
+  
+    return usersSend;
+  }
+  
+
 
 
 // set map options
@@ -34,7 +86,11 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
   }
 initMap();
-
+window.onload = function() {
+    verMejorRuta();
+    //calcRoute();
+};
+console.log(users)
 //create a Directions service object to use the route method and get a result for our request
 // for our requestx|
 var diretionsService = new google.maps.DirectionsService();
@@ -43,18 +99,21 @@ var directionsDisplay = new google.maps.DirectionsRenderer();
 //bind the diretionsRenderer to the Map
 directionsDisplay.setMap(map);
 
-function calcRoute(){
+async function calcRoute(time, destino, nameBranch, numConsult){
+    //numConsult += 1
+    console.log(document.getElementById("users-list").value)
     var request = {
-        origin: document.getElementById("from").value,
-        destination: document.getElementById("to").value,
+        origin: ubicacion,
+        destination: destino,
         travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING AND TRANSIT
         unitSystem: google.maps.UnitSystem.IMPRERIAL
     }
     //Pass the request to the route method
     diretionsService.route(request, (result, status) =>{
         if (status == google.maps.DirectionsStatus.OK){
+            time = result.routes[0].legs[0].duration.text
             //get distance and time
-            const output = document.querySelector('#output');
+            /*const output = document.querySelector('#output');
             output.innerHTML = "<div class='alert-info'>From: " + 
             document.getElementById("from").value +
             ". <br />To: " + 
@@ -63,7 +122,7 @@ function calcRoute(){
             result.routes[0].legs[0].distance.text +
             ". <br />Duration <i class='fas fa-hourglass-start'></i> :" + 
             result.routes[0].legs[0].duration.text +
-            ".</div>";
+            ".</div>";*/
             //display route
             directionsDisplay.setDirections(result);
         } else {
@@ -73,7 +132,26 @@ function calcRoute(){
             //show error message
             output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle-start'></i>Could not retrieve driving distance. </div>";
         }
+        //console.log(time)
+        //console.log(destino)
+        users.push({destino, nameBranch, time});
+        console.log(numConsult)
+        console.log(numConsult == 6)
+        if(numConsult == 6){
+            const usersOrdenados = ordenar(users)
+            console.log(usersOrdenados)
+            //const li = document.createElement('li');
+            for (let i = 0; i < usersOrdenados.length; i++) {
+                const li = document.createElement('li');
+                li.innerText = `Name: ${usersOrdenados[i].nameBranch}, Time: ${usersOrdenados[i].time}`;
+                userList.appendChild(li);
+            }
+            
+        }
+        
     });
+
+    return '';
 }
 
 window.calcRoute = calcRoute
